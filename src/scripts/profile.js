ba553e72c7d98f80b1e930f730e7aa4f1d32ae85
Isaias <isaiascandido92@outlook.com>
2023-02-22 09:17:04 -0300

    essa versão está melhor

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -53,7 +53,6 @@ export function renderRepository() {
 
 
 
-//   console.log(repositories)
 
 
 
@@ -63,49 +62,4 @@ export function renderRepository() {
 
 
 
-//   repositories.map(repository => {
-//     const cardRepos = createRepository(repository)
 
-//   })
-//   mainContainer.appendChild(cardRepos)
-// }
-
-// function createRepository(repository) {
-//   // const repositories = JSON.parse(localStorage.getItem('userRepos'))
-//   // console.log(repositories)
-
-//   const userContainer = document.createElement('div')
-//   userContainer.classList.add('container__user')
-
-//   const user = document.createElement('div')
-//   user.classList.add('user__info')
-
-//   const userAvatar = document.createElement('img')
-//   userAvatar.src = repository.avatar_url
-
-//   const userName = document.createElement('h2')
-//   userName.innerText = repository.login
-
-//   user.append(userAvatar, userName)
-
-//   // const homePageBtn = document.createElement('button')
-//   // homePageBtn.id = 'newSearch'
-//   // homePageBtn.innerText = 'Trocar de usuário'
-
-//   userContainer.append(user)
-
-//   const card = document.createElement('div')
-//   card.classList.add('user__card')
-
-//   const nameProject = document.createElement('h3')
-//   nameProject.innerText = repository.name
-
-//   const description = document.createElement('p')
-//   description.innerText = repository.full_name
-
-//   // const link = document.createElement('a')
-//   // link.href(html_url)
-//   // link.innerText = 'Repositório' 
-
-//   return userContainer
-// }
